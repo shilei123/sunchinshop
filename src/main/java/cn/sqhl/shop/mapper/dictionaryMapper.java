1443d9b40bf849e7659c141839c83237537a130b
Senl <Senl@192.168.0.112>
2017-03-03 14:28:19 +0800

    更新项目文件  上传为项目模式

---
+++ src/main/java/cn/sqhl/shop/mapper/dictionaryMapper.java
@@ -0,0 +1,17 @@
+package cn.sqhl.shop.mapper;
+
+import cn.sqhl.shop.vo.dictionary;
+
+public interface dictionaryMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(dictionary record);
+
+    int insertSelective(dictionary record);
+
+    dictionary selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(dictionary record);
+
+    int updateByPrimaryKey(dictionary record);
+}
